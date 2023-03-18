2495bffc22dc32b2b9e4e20117adf029ab2159c4
Pham Thai Ha HE153262 <73714516+PhamThaiHa153262@users.noreply.github.com>
2023-03-18 15:11:56 +0700

    [common] Initialize commit

---
+++ app.py
@@ -0,0 +1,29 @@
+import flask
+import dao
+from flask import request
+import logging
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+# Create some test data for our catalog in the form of a list of dictionaries.
+
+@app.route('/', methods=['POST', 'GET'])
+def home():
+    return '''<h1>Anomaly detection</h1>
+                <p>A API for interact with MySQL server</p>'''
+
+@app.route('/selectTable1', methods=['POST'])
+def api_id():
+    data = request.get_json()
+    # app.logger.error('%s', type(data['rgb']))
+    result = dao.get_dta_by_id(data['id'])
+    return result
+
+@app.route('/insertTable1', methods=['POST'])
+def insertTable1():
+    data = request.get_json()
+    # app.logger.error('%s', type(data['rgb']))
+    result = dao.innsert_to_tb1(data['name'], data['age'])
+    return result
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=1000)
---
+++ dao.py
@@ -0,0 +1,22 @@
+from mysqlConnection import mydb
+
+
+mycursor = mydb.cursor()
+
+def get_dta_by_id(id):
+    mycursor.execute("SELECT * FROM table1 WHERE id =" + str(id))
+
+    myresult = mycursor.fetchall()
+
+    return myresult
+
+def innsert_to_tb1(name, age):
+    try:
+        print("INSERT INTO table1(name, age) VALUES ('{}', {})".format(name, age))
+        mycursor.execute("INSERT INTO table1(name, age) VALUES ('{}', {})".format(name, age))
+        mydb.commit()
+
+        return "True"
+    except Exception:
+        print(Exception)
+        return "False"
