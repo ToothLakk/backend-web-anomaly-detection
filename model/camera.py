454a3c5910055b5cfe3e16ef3e35dcc011bc0185
Phan Đức Hiếu <phanduchieu@Phans-MacBook-Pro.local>
2023-03-18 19:31:14 +0700

    [common] update folder tree

---
+++ model/camera.py
@@ -0,0 +1,8 @@
+class Camera:
+    def __init__(self, cam_id, cam_name, cam_fps, cam_type):
+        self.cam_id = cam_id
+        self.cam_name = cam_name
+        self.cam_fps = cam_fps
+        self.cam_type = cam_type
+    
+    
