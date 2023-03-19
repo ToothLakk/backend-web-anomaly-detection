85b05f262b225f59e41a5629c656dbea1eca4db3
Chipham0401 <121780791+Chipham0401@users.noreply.github.com>
2023-03-19 21:49:31 +0700

    try an api get user by id

--- app.py
+++ app.py
@@ -6,10 +6,12 @@ app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 # Create some test data for our catalog in the form of a list of dictionaries.
 
+
 @app.route('/', methods=['POST', 'GET'])
 def home():
     return '''<h1>Anomaly detection</h1>
                 <p>A API for interact with MySQL server</p>'''
+
 
 @app.route('/selectTable1', methods=['POST'])
 def api_id():
@@ -18,12 +20,22 @@ def api_id():
     result = dao.get_dta_by_id(data['id'])
     return result
 
+
 @app.route('/insertTable1', methods=['POST'])
 def insertTable1():
     data = request.get_json()
     # app.logger.error('%s', type(data['rgb']))
     result = dao.innsert_to_tb1(data['name'], data['age'])
     return result
+
+
+@app.route('/getuser', methods=['POST'])
+def get_userbyid():
+    data = request.get_json()
+    # app.logger.error('%s', type(data['rgb']))
+    result = dao.getUserById(data['id'])
+    return result
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=1000)
